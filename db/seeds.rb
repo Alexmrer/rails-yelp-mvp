71140f5e6e9d74666e04ec6946cb2ee9596efeab
Alex Meurer <alxmrer@gmail.com>
2021-11-10 15:08:10 +0100

    Restaurants and Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+ciao_ragazzi = { name: 'Ciao Ragazzi', address: 'Türkenstrasse 15, München Maxvorstadt', category: 'italian'}
+muggel = { name: 'Muggel', address: 'Oberkasslerstrasse 32, Düsseldorf Oberkassel', category: 'french'}
+tambosi = { name: 'Tambosi', address: 'Odensplatz 67, München Maxvorstadt', category: 'belgian' }
+brenners = { name: 'Brenners', address: 'Maximilianstrasse 12, München Maxvorstadt', category: 'french' }
+sansibar = { name: 'Sansibar', address: 'Breuningerplatz 90, Düsseldorf Königsallee', category: 'chinese' }
+
+[ciao_ragazzi, muggel, tambosi, brenners, sansibar].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
